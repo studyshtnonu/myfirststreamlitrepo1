a4f1f049fa9bd4628a870788b4d2a785d4acf35c
studyshtnonu <studyshthonu@gmail.com>
2024-10-10 16:10:26 -0400

    Update streamlit_app_multi_select_box.py

--- streamlit_app_multi_select_box.py
+++ streamlit_app_multi_select_box.py
@@ -2,7 +2,10 @@ import streamlit as st
 
 st.header('st.multselect')
 
-option = st.multiselect('What color do you like',['blue','yellow'],['red','green'])
+option = st.multiselect('What color do you like ',
+                        ['blue','yellow'],
+                        ['red','green'])
 
 st.write('selected color is : ',option)
+
 
