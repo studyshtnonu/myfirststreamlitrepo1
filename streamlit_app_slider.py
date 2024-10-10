7ea693c0affaff806111aa742933164558e63f58
studyshtnonu <studyshthonu@gmail.com>
2024-10-10 05:37:36 -0400

    Update streamlit_app_slider.py

--- streamlit_app_slider.py
+++ streamlit_app_slider.py
@@ -5,3 +5,9 @@ st.subheader('st.slider')
 
 age = st.slider('How Old Are You?', 0, 15,25)
 st.write('I am',age,' old')
+
+st.subheader('Range')
+
+values_between = st.slider('select a set of range',0.0,100.0,(25,50))
+st.write(values_between)
+
