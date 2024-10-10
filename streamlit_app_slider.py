8af64a8cdda22e28ee01a46c9509ce693ffe4b91
studyshtnonu <studyshthonu@gmail.com>
2024-10-10 05:53:13 -0400

    Update streamlit_app_slider.py

--- streamlit_app_slider.py
+++ streamlit_app_slider.py
@@ -11,3 +11,7 @@ st.subheader('st.range')
 values_between = st.slider('select a set of range',0.0,100.0,(25.0,50.0))
 st.write(values_between)
 
+st.subheader('range_time')
+appointment = st.slider ('select your appointment', values = (time(11,12),time(2,3)))
+
+st.write('your selected appointment is',appointment)
