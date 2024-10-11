a6ec355a0b8177ef7ab1020bba99980c36e3da7d
studyshtnonu <studyshthonu@gmail.com>
2024-10-11 16:56:04 -0400

    Update streamlitapp_read_csv.py

--- streamlitapp_read_csv.py
+++ streamlitapp_read_csv.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-file_path = 'C:/Users/shail/OneDrive/Desktop/WhisperOpenAI/audio_files/housing.csv'
+file_path = 'C:\\Users\\shail\\OneDrive\\Desktop\\WhisperOpenAI\\audio_files\\housing.csv'
 df = pd.read_csv(file_path)
 print(df.head())
